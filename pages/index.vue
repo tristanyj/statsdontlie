cb4c668a57444e78fe9efb6900fddd6fefac267e
tristanyj <tristanyj.pro@gmail.com>
2024-11-14 23:00:16 +0100

    feat: clean up index.vue by removing commented-out code and improving text clarity

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,6 @@ const { isLoaded } = storeToRefs(configStore);
 <template>
   <div class="grid content">
     <div class="relative">
-      <!-- <VisualisationTest /> -->
       <div class="absolute top-0 left-[10%] h-full w-1 bg-white opacity-[3%]" />
       <div class="absolute top-0 left-[20%] h-full w-1 bg-white opacity-[3%]" />
       <div class="absolute top-0 left-[30%] h-full w-1 bg-white opacity-[3%]" />
@@ -21,20 +20,8 @@ const { isLoaded } = storeToRefs(configStore);
       <div class="absolute top-0 left-[70%] h-full w-1 bg-white opacity-[3%]" />
       <div class="absolute top-0 left-[80%] h-full w-1 bg-white opacity-[3%]" />
       <div class="absolute top-0 left-[90%] h-full w-1 bg-white opacity-[3%]" />
-      <!-- <div class="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full max-w-2xl">
-        <div
-          class="absolute -bottom-4 -left-8 transform -rotate-90 origin-top-left font-normal text-white opacity-25"
-        >
-          LeBron James - Michael Jordan - Kobe Bryant
-        </div>
-      </div> -->
-      <!-- <div class="fixed top-0 left-1/2 -translate-x-1/2 w-full max-w-2xl">
-      </div> -->
       <UContainer class="relative">
-        <!-- <div class="absolute top-0 left-0 w-1 h-[100vh] bg-white" />
-        <div class="absolute top-0 right-0 w-1 h-[100vh] bg-white" /> -->
         <div class="relative py-16 font-anton">
-          <!-- <div class="mb-5"></div> -->
           <div class="flex justify-center relative mb-5">
             <div class="relative">
               <img
@@ -53,21 +40,17 @@ const { isLoaded } = storeToRefs(configStore);
               </div>
             </div>
           </div>
-          <h1 class="uppercase text-white text-center text-8xl">
-            Stats don't lie
-            <!-- <span class="text-7xl">Compare the greatest <br /> </span>
-            <span class="text-5xl"> basketball players of all time </span> -->
-          </h1>
+          <h1 class="uppercase text-white text-center text-8xl">Stats don't lie</h1>
           <div class="mt-8">
-            <div class="grid grid-cols-[1fr,2fr] gap-6 text-justify">
+            <div class="grid grid-cols-2 gap-6 text-justify">
               <p class="font-host text-gray-100">
                 I follow sports for storylines and numbers. I like statistics, records, rivalries. I
-                probably like that better than actually watching the thing. This visualization was
-                created to explore my interest.
+                probably like that better than actually watching the thing. In this visualization, I
+                tried conveying what fascinates me about sports by comparing some of the greatest
+                basketball players of all time.
               </p>
               <p class="font-host text-gray-100 text-justify">
-                Basketball history is filled with great stories and achievements. I collected data
-                for 50 players and 130+ statistics from
+                I collected data for 50 players and 130+ statistics from
                 <a
                   href="https://www.basketball-reference.com/"
                   target="_blank"
@@ -77,30 +60,8 @@ const { isLoaded } = storeToRefs(configStore);
                 >. Stats are mapped around a circle on a scale from usually 0 to the NBA record.
                 Each player has its own color. You can click the center of the chart to choose
                 players and stats to compare.
-
-                <!-- Stats are grouped by categories and types. Each player has its own color. The
-                arc's length is proportional to the player's value in that stat compared to the NBA
-                record. -->
               </p>
-              <!-- <p class="font-host text-gray-50">
-                On a technical note, I used Vue 3, Vite, and Tailwind CSS. Don't try to select more
-                than 50 statistics at the time, it may hurt the chart.
-              </p> -->
             </div>
-            <!-- <div class="grid grid-cols-2 border-4 border-white">
-              <div
-                class="flex justify-center items-center border-r-4 border-white cursor-pointer py-1 transition-color duration-100 hover:bg-white/5"
-                @click="openPicker('players')"
-              >
-                <div class="text-white font-medium uppercase">Select players</div>
-              </div>
-              <div
-                class="flex justify-center items-center cursor-pointer py-1 transition-color duration-100 hover:bg-white/5"
-                @click="openPicker('stats')"
-              >
-                <div class="text-white font-medium uppercase">Select stats</div>
-              </div>
-            </div> -->
           </div>
         </div>
       </UContainer>
@@ -110,7 +71,8 @@ const { isLoaded } = storeToRefs(configStore);
       <UContainer>
         <div class="relative py-16 px-10 text-center lowercase">
           <p class="text-white pl-4">
-            Created by Tristan Lanoye |
+            Created by Tristan Lanoye
+            <span class="relative right-1 mx-3">|</span>
             <a
               href="https://tristanyj.com"
               class="underline"
