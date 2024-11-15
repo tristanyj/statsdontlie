298210b779663d37a11482716f6f22c442583bbd
tristanyj <tristanyj.pro@gmail.com>
2024-11-15 21:14:18 +0100

    fix: update background image path and set HTML language attribute for improved accessibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: SITE_TITLE,
